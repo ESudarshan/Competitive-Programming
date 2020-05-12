105981af2a4081553ac9e19c59ffe723303d8476
Sudarshan Ekambe <sudarshanekambe@gmail.com>
2020-05-12 23:37:20 +0530

    Binary Search

--- LeetCode/May LeetCoding Challenge/SingleElementInASortedArray.java
+++ LeetCode/May LeetCoding Challenge/SingleElementInASortedArray.java
@@ -1,9 +1,26 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int res = 0;
-        for(int num : nums) {
-            res ^= num;
+        if(nums.length == 1) {
+            return nums[0];
+        }      
+        int start = 0;
+        int end = nums.length - 1;
+        if(nums[start] != nums[start+1]) {
+            return nums[start];
+        }      
+        if(nums[end] != nums[end-1]) {
+            return nums[end];
+        }      
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) {
+                return nums[mid];
+            } else if((mid%2 == 0 && nums[mid] == nums[mid+1]) || (mid%2 == 1 && nums[mid] == nums[mid-1])) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
         }
-        return res;
+        return -1;
     }
 }
