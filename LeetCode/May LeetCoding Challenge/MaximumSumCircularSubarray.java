4d7e0686b859bcde5af4243d75dfb24edbb352b8
ESudarshan <sudarshanekambe@gmail.com>
2020-05-18 11:48:46 +0530

    Kadane's Algorithm

--- LeetCode/May LeetCoding Challenge/MaximumSumCircularSubarray.java
+++ LeetCode/May LeetCoding Challenge/MaximumSumCircularSubarray.java
@@ -1,5 +1,21 @@
 class Solution {
     public int maxSubarraySumCircular(int[] A) {
-        
+        int n = A.length;
+        if(n == 1) {
+            return A[0];
+        }
+        int maxSum = Integer.MIN_VALUE;
+        for(int i=0; i<n; i++) {
+            int sum = A[i];
+            int j = (i+1)%n;
+            int counter = 1;
+            while(counter < n){
+                sum = Math.max(sum + A[j], A[j]);
+                maxSum = Math.max(maxSum, sum);
+                j = (j+1)%n;
+                counter++;
+            }
+        }
+        return maxSum;
     }
 }
