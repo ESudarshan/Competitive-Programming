ff6945825afb339dfe526d7daedf766e677f53b2
Sudarshan Ekambe <sudarshanekambe@gmail.com>
2020-05-24 21:38:23 +0530

    SortCharactersByFrequency

--- LeetCode/May LeetCoding Challenge/SortCharactersByFrequency.java
+++ LeetCode/May LeetCoding Challenge/SortCharactersByFrequency.java
@@ -4,44 +4,18 @@ class Solution {
         for(Character ch : s.toCharArray()) {
             map.put(ch, map.getOrDefault(ch, 0) + 1);
         }   
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-        for(char ch : map.keySet()) {
-            pq.add(new Pair(ch, map.get(ch)));
-        }
+        PriorityQueue<Character> pq = new PriorityQueue<>((a, b) -> map.get(a) > map.get(b) ? -1 : map.get(a) == map.get(b) ? 0 : 1);
+        pq.addAll(map.keySet());
         StringBuilder sb = new StringBuilder();
         int n = pq.size();
         for(int i=0; i<n; i++) {
-            Pair p = pq.remove();
-            while(p.cnt != 0) {
-                sb.append(p.ch);
-                p.cnt--;
+            Character ch = pq.remove();
+            Integer cnt = map.get(ch);
+            while(cnt != 0) {
+                sb.append(ch);
+                cnt--;
             }
         }
         return sb.toString();
     }
-}
-
-class Pair implements Comparable<Pair> {
-    char ch;
-    int cnt;
-    
-    Pair(char ch, int cnt) {
-        this.ch = ch;
-        this.cnt = cnt;
-    }
-    
-    public int compareTo(Pair that) {
-        if(this.cnt < that.cnt) {
-            return 1;
-        } else if(this.cnt > that.cnt) {
-            return -1;
-        } else {
-            return 0;
-        }
-    }
-    
-    public String toString() {
-       return ch +"="+cnt;
-    }
-    
 }
