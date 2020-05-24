04993409ede47952bd0c7ee2a5e9e1d42e20034f
Sudarshan Ekambe <sudarshanekambe@gmail.com>
2020-05-24 15:12:58 +0530

    Stock Span

--- LeetCode/May LeetCoding Challenge/OnlineStockSpan.java
+++ LeetCode/May LeetCoding Challenge/OnlineStockSpan.java
@@ -1,22 +1,25 @@
 class StockSpanner {
     
+    Stack<Integer> stack;
     List<Integer> list;
 
     public StockSpanner() {
+        stack = new Stack<>();
         list = new ArrayList<>();
     }
     
     public int next(int price) {
-        int span = 0;
-        list.add(price);
-        for(int i=list.size()-1; i>=0; i--) {
-            int cp = list.get(i);
-            if(cp <= price) {
-                span++;
-            } else {
-                break;
-            }
+        int span = -1;
+        while(!stack.isEmpty() && list.get(stack.peek()) <= price) {
+            stack.pop();
         }
+        if(stack.isEmpty()) {
+            span = list.size() + 1;
+        } else {
+            span = list.size() - stack.peek();
+        }
+        stack.push(list.size());
+        list.add(price);
         return span;
     }
 }
