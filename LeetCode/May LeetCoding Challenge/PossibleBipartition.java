85b89a91e2642f215a58d5a591c0fbfd9a1f10e1
Sudarshan Ekambe <sudarshanekambe@gmail.com>
2020-05-27 23:56:40 +0530

    partial code

---
+++ LeetCode/May LeetCoding Challenge/PossibleBipartition.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean possibleBipartition(int N, int[][] dislikes) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int []a = new int[N];
+        for(int[] d : dislikes) {
+            
+        }
+    }
+}
