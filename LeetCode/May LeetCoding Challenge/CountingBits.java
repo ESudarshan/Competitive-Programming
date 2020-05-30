32558dfcd345274df802ca6054e15c1327438c22
Sudarshan Ekambe <sudarshanekambe@gmail.com>
2020-05-30 23:27:08 +0530

    short and simple

--- LeetCode/May LeetCoding Challenge/CountingBits.java
+++ LeetCode/May LeetCoding Challenge/CountingBits.java
@@ -1,22 +1,8 @@
 class Solution {
     public int[] countBits(int num) {
         int res[]= new int[num+1];
-        res[0] = 0;
-        int p = 0;
-        int i=1;
-        while(i <= num) {
-            int start = (int)Math.pow(2, p);
-            int end = (int)Math.pow(2, p + 1);
-            int diff = (end - start) / 2;
-            while(i <= num && i < end) {    
-                if(i < start + diff) {
-                    res[i] = res[i - diff];
-                } else {
-                    res[i] = res[i - diff] + 1;
-                }
-                i++;
-            }
-            p++;
+        for(int i=0; i<num+1; i++) {
+            res[i] = res[i/2] + (i % 2); 
         }
         return res;
     }
