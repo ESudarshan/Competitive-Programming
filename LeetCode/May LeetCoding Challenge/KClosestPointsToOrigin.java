0d7424cfbb538ae25348180134afa66baf4c2d4e
Sudarshan Ekambe <sudarshanekambe@gmail.com>
2020-06-01 22:23:44 +0530

    Ternary Operator

--- LeetCode/May LeetCoding Challenge/KClosestPointsToOrigin.java
+++ LeetCode/May LeetCoding Challenge/KClosestPointsToOrigin.java
@@ -1,18 +1,10 @@
 class Solution {
     public int[][] kClosest(int[][] points, int K) {
-        Map<int[], Double> map = new HashMap<>();
+        Map<int[], Integer> map = new HashMap<>();
         for(int[] p : points) {
-            map.put(p, Math.sqrt(Math.pow(p[0], 2) + Math.pow(p[1], 2)));
+            map.put(p, p[0] * p[0] + p[1] * p[1]);
         }
-        PriorityQueue<int[]> pq = new PriorityQueue((a, b) -> {
-            if(map.get(a) < map.get(b)){
-                return 1;
-            } else if(map.get(a) > map.get(b)){
-                return -1;
-            } else {
-                return 0;
-            }
-            });
+        PriorityQueue<int[]> pq = new PriorityQueue((a, b) -> map.get(a) < map.get(b) ? 1 : map.get(a) > map.get(b) ? -1 : 0);
         for(int[] p : points) {
             if(pq.size() == K) {
                 if(map.get(p) < map.get(pq.peek())) {
