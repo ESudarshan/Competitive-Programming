fc3ecc002eadf2cf7b02bac23872545c9b764a89
Sudarshan Ekambe <sudarshanekambe@gmail.com>
2020-06-04 21:58:01 +0530

    Max Sum Subarray

---
+++ LeetCode/30-Day LeetCoding Challenge/MaximumSubarray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int maxSum = nums[0];
+        int currentMaxSum = nums[0];
+        for(int i=1; i<n; i++) {
+            currentMaxSum = Math.max(currentMaxSum + nums[i], nums[i]);
+            maxSum = Math.max(maxSum, currentMaxSum);
+        }
+        return maxSum;
+    }
+}
